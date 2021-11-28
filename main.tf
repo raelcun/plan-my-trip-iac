0ade6b9b7595982b4dd093e90beae3706bb4911a
Dan Taylor <dt.raelcun@gmail.com>
2021-11-28 09:54:29 -0500

    got it :)

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "kubernetes_ingress" "plan-my-trip-ingress" {
         }
 
         path {
-          path = "/(index.html|static)"
+          path = "/*"
 
           backend {
             service_name = "frontend-service"
